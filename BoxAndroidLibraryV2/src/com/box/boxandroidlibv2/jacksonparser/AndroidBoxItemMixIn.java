1981bd974c64c7818cfbc639e7073ff30d1ce478
Rico Yao <ryao@box.net>
2013-05-02 11:02:50 -0700

    BoxRealtimeServer should not be in AndroidBoxItemMixIn.

--- BoxAndroidLibraryV2/src/com/box/boxandroidlibv2/jacksonparser/AndroidBoxItemMixIn.java
+++ BoxAndroidLibraryV2/src/com/box/boxandroidlibv2/jacksonparser/AndroidBoxItemMixIn.java
@@ -4,7 +4,6 @@ import com.box.boxandroidlibv2.dao.BoxAndroidEvent;
 import com.box.boxandroidlibv2.dao.BoxAndroidFile;
 import com.box.boxandroidlibv2.dao.BoxAndroidFolder;
 import com.box.boxandroidlibv2.dao.BoxAndroidWebLink;
-import com.box.boxjavalibv2.dao.BoxRealTimeServer;
 import com.box.boxjavalibv2.interfaces.IJacksonMixIn;
 import com.fasterxml.jackson.annotation.JsonSubTypes;
 import com.fasterxml.jackson.annotation.JsonSubTypes.Type;
@@ -15,8 +14,7 @@ import com.fasterxml.jackson.annotation.JsonTypeInfo;
  */
 @JsonTypeInfo(use = JsonTypeInfo.Id.NAME, include = JsonTypeInfo.As.PROPERTY, property = "type")
 @JsonSubTypes({@Type(value = BoxAndroidFile.class, name = "file"), @Type(value = BoxAndroidFolder.class, name = "folder"),
-               @Type(value = BoxAndroidWebLink.class, name = "web_link"), @Type(value = BoxAndroidEvent.class, name = "event"),
-               @Type(value = BoxRealTimeServer.class, name = "realtime_server")})
+               @Type(value = BoxAndroidWebLink.class, name = "web_link"), @Type(value = BoxAndroidEvent.class, name = "event")})
 public class AndroidBoxItemMixIn implements IJacksonMixIn {
 
 }
