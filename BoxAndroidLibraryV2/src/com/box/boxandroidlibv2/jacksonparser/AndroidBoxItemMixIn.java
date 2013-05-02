ff81e335b0eef7eafc43212a5e98ecd0ed18027f
Rico Yao <ryao@box.net>
2013-05-01 19:53:41 -0700

    Add support for Box Events API.

--- BoxAndroidLibraryV2/src/com/box/boxandroidlibv2/jacksonparser/AndroidBoxItemMixIn.java
+++ BoxAndroidLibraryV2/src/com/box/boxandroidlibv2/jacksonparser/AndroidBoxItemMixIn.java
@@ -1,8 +1,10 @@
 package com.box.boxandroidlibv2.jacksonparser;
 
+import com.box.boxandroidlibv2.dao.BoxAndroidEvent;
 import com.box.boxandroidlibv2.dao.BoxAndroidFile;
 import com.box.boxandroidlibv2.dao.BoxAndroidFolder;
-import com.box.boxjavalibv2.dao.BoxWebLink;
+import com.box.boxandroidlibv2.dao.BoxAndroidWebLink;
+import com.box.boxjavalibv2.dao.BoxRealTimeServer;
 import com.box.boxjavalibv2.interfaces.IJacksonMixIn;
 import com.fasterxml.jackson.annotation.JsonSubTypes;
 import com.fasterxml.jackson.annotation.JsonSubTypes.Type;
@@ -12,7 +14,9 @@ import com.fasterxml.jackson.annotation.JsonTypeInfo;
  * A jackson <a href="http://wiki.fasterxml.com/JacksonMixInAnnotations">MixIn</a> class to direct the parsing of BoxItem types into proper sub type.
  */
 @JsonTypeInfo(use = JsonTypeInfo.Id.NAME, include = JsonTypeInfo.As.PROPERTY, property = "type")
-@JsonSubTypes({@Type(value = BoxAndroidFile.class, name = "file"), @Type(value = BoxAndroidFolder.class, name = "folder"),@Type(value = BoxWebLink.class, name = "web_link")})
+@JsonSubTypes({@Type(value = BoxAndroidFile.class, name = "file"), @Type(value = BoxAndroidFolder.class, name = "folder"),
+               @Type(value = BoxAndroidWebLink.class, name = "web_link"), @Type(value = BoxAndroidEvent.class, name = "event"),
+               @Type(value = BoxRealTimeServer.class, name = "realtime_server")})
 public class AndroidBoxItemMixIn implements IJacksonMixIn {
 
 }
