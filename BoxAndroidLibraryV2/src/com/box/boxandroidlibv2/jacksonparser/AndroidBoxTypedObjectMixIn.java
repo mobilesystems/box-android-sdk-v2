dc4f4fee95396941fee992314a0bed8931cbd8de
jian lin <jian@box.com>
2013-05-07 14:52:32 -0700

    Fix an issue that web link objects in a collection are parsed into BoxWebLink instead of BoxAndroidWebLink

--- BoxAndroidLibraryV2/src/com/box/boxandroidlibv2/jacksonparser/AndroidBoxTypedObjectMixIn.java
+++ BoxAndroidLibraryV2/src/com/box/boxandroidlibv2/jacksonparser/AndroidBoxTypedObjectMixIn.java
@@ -8,9 +8,9 @@ import com.box.boxandroidlibv2.dao.BoxAndroidFile;
 import com.box.boxandroidlibv2.dao.BoxAndroidFileVersion;
 import com.box.boxandroidlibv2.dao.BoxAndroidFolder;
 import com.box.boxandroidlibv2.dao.BoxAndroidUser;
+import com.box.boxandroidlibv2.dao.BoxAndroidWebLink;
 import com.box.boxjavalibv2.dao.BoxRealTimeServer;
 import com.box.boxjavalibv2.dao.BoxServerError;
-import com.box.boxjavalibv2.dao.BoxWebLink;
 import com.box.boxjavalibv2.interfaces.IJacksonMixIn;
 import com.fasterxml.jackson.annotation.JsonSubTypes;
 import com.fasterxml.jackson.annotation.JsonSubTypes.Type;
@@ -21,7 +21,7 @@ import com.fasterxml.jackson.annotation.JsonTypeInfo;
  */
 @JsonTypeInfo(use = JsonTypeInfo.Id.NAME, include = JsonTypeInfo.As.PROPERTY, property = "type")
 @JsonSubTypes({@Type(value = BoxAndroidFile.class, name = "file"), @Type(value = BoxAndroidFolder.class, name = "folder"),
-               @Type(value = BoxWebLink.class, name = "web_link"), @Type(value = BoxAndroidCollaboration.class, name = "collaboration"),
+               @Type(value = BoxAndroidWebLink.class, name = "web_link"), @Type(value = BoxAndroidCollaboration.class, name = "collaboration"),
                @Type(value = BoxAndroidComment.class, name = "comment"), @Type(value = BoxAndroidEmailAlias.class, name = "email_alias"),
                @Type(value = BoxAndroidFileVersion.class, name = "file_version"), @Type(value = BoxAndroidUser.class, name = "user"),
                @Type(value = BoxServerError.class, name = "error"), @Type(value = BoxAndroidEvent.class, name = "event"),
